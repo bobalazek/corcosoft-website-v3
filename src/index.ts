000d09d6616a1c7dceadb36608870009fb3723e2
Borut <bobalazek124@gmail.com>
2020-09-13 13:40:03 +0200

    New css stuff

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
+import jQuery from 'jquery';
+
 // CSS
 import './css/website.scss';
+
+// jQuery
+jQuery(document).ready(function($) {
+  $('[data-animation]').each(function(i) {
+    var $el = $(this);
+    var animation = $el.attr('data-animation');
+    setTimeout(function () {
+      $el
+        .css('visibility', 'visible')
+        .addClass('animate__animated animate__' + animation)
+      ;
+    }, i * 400);
+  });
+});
