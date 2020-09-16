8ff0430ffbf6f78072aea2c17e6f728d83fc20fe
Borut <bobalazek124@gmail.com>
2020-09-16 09:55:35 +0200

    WIP

--- src/game/index.ts
+++ src/game/index.ts
@@ -3,16 +3,17 @@ import {
   Scene,
   AbstractMesh,
   UniversalCamera,
-  PBRMetallicRoughnessMaterial,
   CubeTexture,
   SceneLoader,
+  Animation,
+  Quaternion,
   Vector3,
   Color4,
 } from 'babylonjs';
 import 'babylonjs-loaders';
 
 // Constants
-const CAMERA_DISTANCE = 15;
+const CAMERA_DISTANCE = 25;
 const CHARACTER_POSITION_SMOOOTHING = 0.002;
 
 // CSS
@@ -35,7 +36,8 @@ var environmentTexture = CubeTexture.CreateFromPrefilteredData(
 );
 scene.environmentTexture = environmentTexture;
 
-scene.debugLayer.show({ overlay: true });
+// Game - Scene - Debug
+//scene.debugLayer.show({ overlay: true });
 
 // Game - Scene - Camera
 let camera = new UniversalCamera(
@@ -46,12 +48,6 @@ let camera = new UniversalCamera(
 camera.lockedTarget = new Vector3(0, 0, 0);
 
 // Game - Scene - Character
-let characterMaterial = new PBRMetallicRoughnessMaterial(
-  'characterMaterial',
-  scene
-);
-characterMaterial.environmentTexture = environmentTexture;
-
 let character: AbstractMesh = null;
 let characterFinalPosition = new Vector3();
 
@@ -60,10 +56,18 @@ SceneLoader.LoadAssetContainer('/static/media/models/', 'bot.glb', scene, functi
 
   character = scene.getMeshByID('__root__');
   character.id = character.name = 'CorcobotWrapper';
+  character.position = new Vector3(0, 10, 0);
 
-  character.scaling = Vector3.Zero();
-
-  // TODO: animate scale
+  Animation.CreateAndStartAnimation(
+    'CorcobotWrapperScale',
+    character,
+    'scaling',
+    60,
+    300,
+    Vector3.Zero(),
+    Vector3.One(),
+    Animation.ANIMATIONLOOPMODE_CONSTANT
+  );
 });
 
 // Game - Render loop
@@ -75,6 +79,8 @@ engine.runRenderLoop(() => {
       characterFinalPosition,
       engine.getDeltaTime() * CHARACTER_POSITION_SMOOOTHING
     );
+
+    // TODO: rotate towards direction
   }
 
   // Render
@@ -94,3 +100,16 @@ window.addEventListener('mousemove', (e) => {
     pickResult.ray.origin.z + pickResult.ray.direction.z * CAMERA_DISTANCE
   );
 });
+
+// Game - Functions
+function lookAt(from: Vector3, to: Vector3, front: Vector3, up: Vector3) {
+  const direction: Vector3 = from.subtract(to).normalize();
+  let rotationAxis: Vector3 = from.cross(direction).normalize();
+  if (rotationAxis.lengthSquared() === 0) {
+    rotationAxis = up;
+  }
+
+  const angle: number = Vector3.Dot(Vector3.Forward(), direction);
+
+  return Quaternion.RotationAxis(rotationAxis, angle);
+}
