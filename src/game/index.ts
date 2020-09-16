2bcc729464e3a2fe85d53911a16e16a5f007ca64
Borut <bobalazek124@gmail.com>
2020-09-16 15:51:59 +0200

    More calculations

--- src/game/index.ts
+++ src/game/index.ts
@@ -6,8 +6,8 @@ import {
   CubeTexture,
   SceneLoader,
   Animation,
+  Matrix,
   Quaternion,
-  Axis,
   Vector3,
   Color4,
 } from 'babylonjs';
@@ -16,24 +16,23 @@ import 'babylonjs-loaders';
 // Constants
 const CAMERA_DISTANCE = 25;
 const CHARACTER_POSITION_SMOOOTHING = 0.002;
-const WORLD_FORWARD = new Vector3(0, 0, 1);
-const WORLD_UP = new Vector3(0, 1, 0);
+const CHARACTER_ROTATION_SMOOOTHING = 0.002;
 
 // CSS
 import './css/index.scss';
 
-// Game
+/********** Game **********/
 const canvas = <HTMLCanvasElement>document.getElementById('canvas');
 const engine = new Engine(
   canvas,
   true
 );
 
-// Game - Scene
+/********** Game - Scene **********/
 let scene = new Scene(engine);
 scene.clearColor = new Color4(0, 0, 0, 0);
 
-var environmentTexture = CubeTexture.CreateFromPrefilteredData(
+let environmentTexture = CubeTexture.CreateFromPrefilteredData(
   'static/media/textures/environment.dds',
   scene
 );
@@ -53,41 +52,24 @@ camera.lockedTarget = new Vector3(0, 0, 0);
 // Game - Scene - Character
 let character: AbstractMesh = null;
 let characterFinalPosition = new Vector3();
+let characterFinalRotationQuaternion = new Quaternion();
 
-SceneLoader.LoadAssetContainer('/static/media/models/', 'bot.glb', scene, function (container) {
-  container.addAllToScene();
+prepareCharacter();
 
-  character = scene.getMeshByID('__root__');
-  character.id = character.name = 'CorcobotWrapper';
-  character.position = new Vector3(0, 10, 0);
-
-  Animation.CreateAndStartAnimation(
-    'CorcobotWrapperScale',
-    character,
-    'scaling',
-    60,
-    300,
-    Vector3.Zero(),
-    Vector3.One(),
-    Animation.ANIMATIONLOOPMODE_CONSTANT
-  );
-});
-
-// Game - Render loop
+/********** Game - Render loop **********/
 engine.runRenderLoop(() => {
-  // Character movement
   if (character) {
+    calculateCharacterRotation();
+
     character.position = Vector3.Lerp(
       character.position,
       characterFinalPosition,
       engine.getDeltaTime() * CHARACTER_POSITION_SMOOOTHING
     );
-
-    character.rotationQuaternion = lookAt(
-      character.position,
-      characterFinalPosition,
-      WORLD_FORWARD,
-      WORLD_UP
+    character.rotationQuaternion = Quaternion.Slerp(
+      character.rotationQuaternion,
+      characterFinalRotationQuaternion,
+      engine.getDeltaTime() * CHARACTER_ROTATION_SMOOOTHING
     );
   }
 
@@ -95,32 +77,66 @@ engine.runRenderLoop(() => {
   scene.render();
 });
 
-// Game - Events
+/********** Game - Events **********/
 window.addEventListener('resize', () => {
   engine.resize();
 });
 
 window.addEventListener('mousemove', (e) => {
-  const pickResult = scene.pick(e.clientX, e.clientY);
+  calculateCharacterPosition(e.clientX, e.clientY);
+});
+
+/********** Game - Functions **********/
+function prepareCharacter() {
+  SceneLoader.LoadAssetContainer('/static/media/models/', 'bot.glb', scene, function (container) {
+    container.addAllToScene();
+
+    character = scene.getMeshByID('__root__');
+    character.id = character.name = 'CorcobotWrapper';
+    character.position = new Vector3(0, 10, 0);
+
+    Animation.CreateAndStartAnimation(
+      'CorcobotWrapperScale',
+      character,
+      'scaling',
+      60,
+      300,
+      Vector3.Zero(),
+      Vector3.One(),
+      Animation.ANIMATIONLOOPMODE_CONSTANT
+    );
+  });
+}
+
+function calculateCharacterPosition(screenX, screenY) {
+  const pickResult = scene.pick(screenX, screenY);
   characterFinalPosition = new Vector3(
     pickResult.ray.origin.x + pickResult.ray.direction.x * CAMERA_DISTANCE,
     pickResult.ray.origin.y + pickResult.ray.direction.y * CAMERA_DISTANCE,
     pickResult.ray.origin.z + pickResult.ray.direction.z * CAMERA_DISTANCE
   );
-});
+}
 
-// Game - Functions
 // https://stackoverflow.com/a/51170230/4642875
-function lookAt(from: Vector3, to: Vector3, front: Vector3, up: Vector3) {
+function calculateCharacterRotation() {
+  const from = character.position;
+  const to = characterFinalPosition;
+
+  const distance = Vector3.DistanceSquared(to, from);
   const direction: Vector3 = to.subtract(from).normalize();
 
-  let rotationAxis: Vector3 = front.cross(direction).normalize();
+  let rotationAxis: Vector3 = (Vector3.Forward()).cross(direction).normalize();
   if (rotationAxis.lengthSquared() === 0) {
-    rotationAxis = up;
+    rotationAxis = Vector3.Up();
   }
 
   const dot: number = Vector3.Dot(Vector3.Forward(), direction);
   const angle = Math.acos(dot);
 
-  return Quaternion.RotationAxis(rotationAxis, angle);
+  characterFinalRotationQuaternion = Quaternion.RotationAxis(rotationAxis, angle);
+
+  // TODO: smoother transition - crossmultiply?
+  if (distance < 2) {
+    characterFinalRotationQuaternion = Quaternion.FromEulerAngles(0, Math.PI, 0);
+  }
 }
